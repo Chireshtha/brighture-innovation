93344a12c2e989db4ae8845bef9c2b4e7e4512c0
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-11 22:42:02 +0530

    first commit

--- src/components/forms/SubscribePageForm.js
+++ src/components/forms/SubscribePageForm.js
@@ -25,7 +25,7 @@ const SubscribePageForm = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/subscribe`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/subscribe/${id}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(values),
